9ec2eb3aedb434fe0e79c78f091b9377f9269117
salimalitarikz <salimalitarikz@gmail.com>
2024-11-01 12:40:11 +0300

    d1-u2-v2

--- site.js
+++ site.js
@@ -1,3 +1,12 @@
+// functions 
+
+function qs(query) {
+    return document.querySelector(query);
+    
+}
+
+
+
 function menuToggle() {
     let menu = document.getElementById("menu");
     let main = document.getElementById("main");
@@ -5,3 +14,33 @@ function menuToggle() {
     menu.classList.toggle("menu_active");
     main.classList.toggle("main_disabled");
 }
+
+
+
+
+
+
+window.addEventListener("scroll", (el) => {
+
+    el.preventDefault();
+
+
+    let scrollVal = window.scrollTop;
+
+    console.log("---> " + scrollVal);
+
+
+
+
+    // question appears then becomes white colored
+    if (scrollVal > 200) {
+        qs("#globe").classList.add("activated");
+    }else{
+        qs("#globe").classList.remove("activated");        
+    }
+
+   
+
+
+
+});
